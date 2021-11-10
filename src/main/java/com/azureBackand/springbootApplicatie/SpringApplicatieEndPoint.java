813b3959e8f7feccb3d212fe40cb7131a21cf3c2
Eva Bouwman <eva.milou@gmail.com>
2021-11-10 16:00:02 +0100

    crossorigin

--- src/main/java/com/azureBackand/springbootApplicatie/SpringApplicatieEndPoint.java
+++ src/main/java/com/azureBackand/springbootApplicatie/SpringApplicatieEndPoint.java
@@ -1,9 +1,11 @@
 package com.azureBackand.springbootApplicatie;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(maxAge=3600)
 public class SpringApplicatieEndPoint {
 	@GetMapping("endpoint1")
 	public Person daarGaanWe() {
